91dda53c25688b1d231e1cb57049e6fe6f4e3de3
LuckyMasharane <lmasharane@gmail.com>
2020-09-16 07:43:13 +0200

    3 files modified

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeesService } from '../employees.service';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent implements OnInit {
+     message: string = "Employee Details"
+    public contactForm: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder, private employeeService: EmployeesService,) { }
 
-  ngOnInit() {
+ ngOnInit() {
+   this.contactForm = this._formBuilder.group({
+     id: 10,
+      empName: "",
+      empSurname: "",
+      empPosition: "",
+      empAge: "",
+      empImage: ""
+    });
   }
-
+  onSubmit() {
+    this.employeeService.addEmployee(this.contactForm.value);
+  }
 }
